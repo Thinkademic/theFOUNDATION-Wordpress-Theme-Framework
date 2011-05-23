8aef9679fb6849f5a6bfbb81404f2cfedae393db
BaoQuocDoan <wok@foodshelterlove.com>
2011-05-22 17:04:20 -0700

    functions-social-media.php More edits
    Signed-off-by: BaoQuocDoan <wok@foodshelterlove.com>

--- functions/functions-social-media.php
+++ functions/functions-social-media.php
@@ -21,7 +21,8 @@ function get_thefdt_social_media_box_count() {
 	$box_count .= get_twitter_box_count();
 	
 	$box_count = xtag("div", $box_count, "class=social_media_box_count");
- 
+    $box_count = xtag("div", $box_count, "class=social_media_box_count_wrapper");
+	
 	return $box_count;
 }
  
@@ -32,9 +33,11 @@ function get_thefdt_social_media_box_count() {
 function get_facebook_box_count() {
 
 	$permalink = urlencode(get_permalink($post->ID));
-
-	return '<iframe src="http://www.facebook.com/plugins/like.php?href='.$permalink.'&amp;layout=box_count&amp;show_faces=false&amp;width=60&amp;action=like&amp;colorscheme=light&amp;font=arial" scrolling="no" frameborder="1" allowTransparency="true" style="border:none; overflow:hidden; width:60px; height:65px;"></iframe>';
-		
+	$facebook =  '<iframe src="http://www.facebook.com/plugins/like.php?href='.$permalink.'&amp;layout=box_count&amp;show_faces=false&amp;width=60&amp;action=like&amp;colorscheme=light&amp;font=arial" scrolling="no" frameborder="0" allowTransparency="true" style="border:none; overflow:hidden; width:60px; height:65px;"></iframe>';
+	$facebook = xtag("div", $facebook, "class=facebook_box_count");
+	
+	return $facebook;
+	
 }
 
 /*
@@ -43,24 +46,15 @@ function get_facebook_box_count() {
 function get_twitter_box_count() {
 	global $post, $twitter_active;
 
- 	$permalink = urlencode(get_permalink($post->ID));
+ 	$permalink = get_permalink($post->ID);
 	$post_title = get_the_title($post->ID);
-	$short_link = urlencode(get_permalink($post->ID));
-	$username = "twitteruser";
-	$text = 'Checking out this page about Tweet Buttons';
-	
-	if(!$twitter_active) :
-	 $script = '<script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>';
-	 $twitter_active = false;
-	else :
-	 $twitter_active = true;
-	endif;
-	
+	$short_link = wp_get_shortlink($post->ID);
+	$username = of_get_option('social_media_twitter_profile', '');
+	$text = "I'd sport it...would you?";
+		
 	return '
-	
-			<div>
+			<div class="twitter_box_count">
 			  <a href="http://twitter.com/share" class="twitter-share-button"
-				 data-counturl="'.$post_title.'"
 				 data-via="'. $username .'"
 				 data-text="'. $text .'"
 				 data-url="'. $short_link .'"
